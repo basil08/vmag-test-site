90664b1ec70f742d29a0fba8b5f46597405e6d3f
Basil Labib <basillabib01@gmail.com>
2021-10-11 19:44:22 +0530

    Remove import for masonry

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import '../styles/scss/globals.scss'
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle");
-    import("../js/masonry.min");
   })
   return <Component {...pageProps} />
 }
